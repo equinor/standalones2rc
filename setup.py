2aaf7c5733038a9213a2e05693131c7f073cad15
Anders Fredrik Kiær <akia@statoil.com>
2019-12-09 10:55:26 +0100

    Add script

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+TESTS_REQUIRES = [
+    "pylint~=2.3",
+    "black",
+    "bandit",
+]
+
+setup(
+    name="standalones2rc",
+    description="Automatically join different reservoir simulations into one coupled model",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/equinor/standalones2rc",
+    author="R&T Equinor",
+    packages=find_packages(exclude=["tests"]),
+    entry_points={"console_scripts": ["standalones2rc=standalones2rc:main"]},
+    install_requires=[
+    ],
+    tests_require=TESTS_REQUIRES,
+    extras_require={"tests": TESTS_REQUIRES},
+    setup_requires=["setuptools_scm~=3.2"],
+    use_scm_version=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    ],
+)
